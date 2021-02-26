6fb0138a558f93cc82318eda9b99b1d056851fe4
keite passos <keitepassos@gmail.com>
2021-02-26 11:40:03 -0300

    .

---
+++ api/src/controllers/UserController.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import { User } from "../models/User";
+
+class UserController {
+    async create(request: Request, response: Response) {
+        const { name, email } = request.body;
+
+        const usersRepository = getRepository(User);
+
+        const userAlreadyExists = await usersRepository.findOne({
+            email
+        });
+
+        if(userAlreadyExists) {
+            return response.status(400).json({
+                error: "User already exists!",
+            });
+        }
+
+        const user = usersRepository.create({
+            name, email
+        });
+
+        await usersRepository.save(user);
+
+        return response.send(user);
+    }
+
+    async ready(request: Request, response: Response){
+        return response.json({message:"Lista"});
+    }
+}
+
+export { UserController }
